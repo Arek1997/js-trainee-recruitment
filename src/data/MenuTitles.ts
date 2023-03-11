17998ec3177591c819132f1e306f639fbf086146
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 13:15:00 +0100

    Add data folder

---
+++ src/data/MenuTitles.ts
@@ -0,0 +1,9 @@
+export const menuTitles = [
+	'Polar Bear',
+	'Cheetah',
+	'Panda',
+	'Fox',
+	'Squirrel',
+	'Butterfly',
+	'Elephant',
+];
---
+++ src/data/Slides.ts
@@ -0,0 +1,44 @@
+export const slides = [
+	{
+		subtitle: 'POLAR BEAR',
+		title: 'Say hello to your new',
+		featured: 'friend',
+		imagePath: '/images/polar-bear.jpg',
+	},
+	{
+		subtitle: 'CHEETAH',
+		title: 'No petting before',
+		featured: 'eating',
+		imagePath: '/images/cheetah.jpg',
+	},
+	{
+		subtitle: 'PANDA',
+		title: 'Eating always with',
+		featured: 'pleasure',
+		imagePath: '/images/panda.jpg',
+	},
+	{
+		subtitle: 'FOX',
+		title: 'Sometimes quite',
+		featured: 'suspicious',
+		imagePath: '/images/fox.jpg',
+	},
+	{
+		subtitle: 'SQUIRREL',
+		title: 'Staying',
+		featured: 'curious',
+		imagePath: '/images/squirrel.jpg',
+	},
+	{
+		subtitle: 'BUTTERFLY',
+		title: 'every time of a day',
+		featured: 'Majestic',
+		imagePath: '/images/butterfly.jpg',
+	},
+	{
+		subtitle: 'ELEPHANT',
+		title: 'It makes a  difference',
+		featured: 'huge',
+		imagePath: '/images/elephant.jpg',
+	},
+];
