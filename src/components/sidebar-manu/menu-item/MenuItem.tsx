630e08a8b9e822606d1c1d27e25d945916c18bc5
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 16:54:52 +0100

    Add on click method, add more interface props

--- src/components/sidebar-manu/menu-item/MenuItem.tsx
+++ src/components/sidebar-manu/menu-item/MenuItem.tsx
@@ -1,19 +1,23 @@
+import { transformText } from '@/helpers/functions';
+import { Dispatch, SetStateAction } from 'react';
+
 interface Props {
 	title: string;
-	active?: boolean;
+	activeLink: string;
+	setActiveLink: Dispatch<SetStateAction<string>>;
 }
 
-const MenuItem = ({ title, active }: Props) => {
-	const linkPath = title.toLowerCase().replaceAll(' ', '-');
-
-	const activeLink = active ? 'active' : '';
+const MenuItem = ({ title, activeLink, setActiveLink }: Props) => {
+	const linkPath = `#${transformText(title)}`;
+	const active = linkPath.includes(activeLink) ? 'active' : '';
 
 	return (
 		<li>
 			<a
-				href={`#${linkPath}`}
-				className={`block px-14 py-[10px] text-[14px] font-medium opacity-70 ${activeLink} transition-colors hover:bg-[var(--active-bg-color)] hover:text-[var(--active-color)]`}
+				href={linkPath}
+				className={`block px-14 py-[10px] text-[14px] font-medium opacity-70 ${active} transition-colors hover:bg-[var(--active-bg-color)] hover:text-[var(--active-color)]`}
 				data-section={linkPath}
+				onClick={() => setActiveLink(linkPath)}
 			>
 				{title}
 			</a>
