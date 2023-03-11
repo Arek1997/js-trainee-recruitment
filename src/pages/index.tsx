081e85b2446e86dbd353a65295abf7030de11c59
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 12:38:58 +0100

    Add tailwind / config, add background to body, add colors variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import SidebarMenu from '@/components/sidebar-manu/SidebarMenu';
+import Slides from '@/components/slides/Slides';
+
 export default function Home() {
-	return <div className='my-[15%] mx-auto max-w-[720px] text-center'></div>;
+	return (
+		<div className='my-[15%] mx-auto flex max-w-[720px] gap-x-20 text-center'>
+			<SidebarMenu />
+			<Slides />
+		</div>
+	);
 }
