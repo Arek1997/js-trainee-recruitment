dbbe689d19e15eb890e85c38f4d8d6f7d9a4e375
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 09:16:08 +0100

    Add tailwind and sidebar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-import { LogoNerdbord } from "../components/LogoNerdbord";
-
-import styles from "../styles/index.module.css";
-
 export default function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <LogoNerdbord />
-      <h1 className={inter.className}>
-        Recruitment task for Javascript Trainee
-      </h1>
-    </div>
-  );
+	return <div className='my-[15%] mx-auto max-w-[720px] text-center'></div>;
 }
