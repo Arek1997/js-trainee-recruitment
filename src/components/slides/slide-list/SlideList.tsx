d45039db5877d960280004b65f24916e877d297a
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 16:53:14 +0100

    Render slide list

--- src/components/slides/slide-list/SlideList.tsx
+++ src/components/slides/slide-list/SlideList.tsx
@@ -1,7 +1,15 @@
+import { slides } from '@/data/Slides';
 import React from 'react';
+import SlideItem from '../slide-item/SlideItem';
 
 const SlideList = () => {
-	return <div>SlideList</div>;
+	return (
+		<>
+			{slides.map((slide) => (
+				<SlideItem key={Math.random() * 20 + slide.subtitle} {...slide} />
+			))}
+		</>
+	);
 };
 
 export default SlideList;
